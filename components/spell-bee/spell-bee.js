602075de284605259d8e990348101a36bace7587
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2019-07-17 17:26:48 +1000

    styling

--- components/spell-bee/spell-bee.js
+++ components/spell-bee/spell-bee.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import vocabulator from 'vocabulator';
-import { last, randWords } from './words';
+import { combinedAll, randWords } from './words';
 import css from './spell-bee.scss';
 
 const synthesizer = vocabulator({
@@ -12,7 +12,8 @@ const synthesizer = vocabulator({
 const modes = [
   {
     label: 'Random 10',
-    value: 1
+    value: 1,
+    selected: true
   },
   {
     label: 'Random 20',
@@ -31,9 +32,8 @@ const modes = [
     value: 10
   },
   {
-    label: 'Last',
-    value: 0,
-    selected: true
+    label: 'All 1000',
+    value: 0
   }
 ];
 
@@ -42,19 +42,19 @@ const CORRECT = 'correct';
 const WRONG = 'wrong';
 
 const SpellBee = () => {
-  const [wordList, setWordList] = useState(last);
-  const [modeNum, setModeNum] = useState(0);
+  const [wordList, setWordList] = useState(randWords(1));
+  const [modeNum, setModeNum] = useState(1);
   const [wordCount, setWordCount] = useState(0);
   const [currentWord, setCurrentWord] = useState('');
   const [numCorrect, setNumCorrect] = useState(0);
   const [currentAnswerStatus, setCurrentAnswerStatus] = useState(UNATTEMPTED);
   const [checkedDisable, setCheckDisable] = useState(false);
-  // const [attemptedWords, setAttemptedWords] = useState([]);
+  const [timeline, setTimeline] = useState([]);
   const [isComplete, setIsComplete] = useState(false);
 
   const init = (mode) => {
     if (Number(mode) === 0) {
-      setWordList(last);
+      setWordList(combinedAll);
     } else {
       setWordList(randWords(mode));
     }
@@ -63,6 +63,9 @@ const SpellBee = () => {
     setCurrentWord('');
     setCurrentAnswerStatus(UNATTEMPTED);
     setIsComplete(false);
+    setCheckDisable(false);
+    setNumCorrect(0);
+    setTimeline([]);
   };
 
   const handleSay = () => {
@@ -77,8 +80,10 @@ const SpellBee = () => {
     if (currentWord === wordList[wordCount]) {
       setNumCorrect(numCorrect + 1);
       setCurrentAnswerStatus(CORRECT);
+      setTimeline([...timeline, { word: wordList[wordCount], isCorrect: true }]);
     } else {
       setCurrentAnswerStatus(WRONG);
+      setTimeline([...timeline, { word: wordList[wordCount], isCorrect: false }]);
     }
   };
 
@@ -89,72 +94,84 @@ const SpellBee = () => {
     setCheckDisable(false);
   };
 
-  useEffect(() => {
-    // console.log(wordList);
-  }, [wordList]);
+  // useEffect(() => {
+  //   console.log(timeline);
+  // }, [timeline]);
 
   return (
     <div className={css['spell-bee']}>
-      <div className={css.header}>
-        <select
-          onChange={e => init(e.target.value)}
-        >
-          {
-            modes.map(mode => (
-              <option
-                defaultValue={mode.defaultValue ? mode.value : false}
-                value={mode.value}
-                key={mode.label}
-              >
-                {mode.label}
-              </option>
-            ))
-          }
-        </select>
-        <button
-          type="button"
-          onClick={() => init(modeNum)}
-          disabled={!isComplete}
-        >
-          Start again
-        </button>
-      </div>
-      <div className={css.content}>
-        <p>{`Score: ${numCorrect} / ${wordList.length} | Attempt ${wordCount + 1} / ${wordList.length}`}</p>
-        <button
-          type="button"
-          onClick={handleSay}
-          disabled={checkedDisable}
-        >
-          Say the word
-        </button>
-
-        <div className={css.answer}>
-          <input
-            type="text"
-            onChange={e => setCurrentWord(e.target.value)}
-            value={currentWord}
-          />
-          <p>{currentAnswerStatus === UNATTEMPTED ? '' : currentAnswerStatus}</p>
+      <div className={css.container}>
+        <div className={css.header}>
+          <select
+            onChange={e => init(e.target.value)}
+          >
+            {
+              modes.map(mode => (
+                <option
+                  defaultValue={mode.defaultValue ? mode.value : false}
+                  value={mode.value}
+                  key={mode.label}
+                >
+                  {mode.label}
+                </option>
+              ))
+            }
+          </select>
+          <button
+            type="button"
+            onClick={() => init(modeNum)}
+            disabled={!isComplete}
+          >
+            Start again
+          </button>
         </div>
+        <div className={css.content}>
+          <p>{`Score: ${numCorrect} / ${wordList.length} | Attempt ${wordCount + 1} / ${wordList.length}`}</p>
+          <button
+            type="button"
+            onClick={handleSay}
+          >
+            Say the word
+          </button>
 
-        <p>{currentAnswerStatus === WRONG ? wordList[wordCount] : ''}</p>
+          <div className={css.answer}>
+            <input
+              type="text"
+              onChange={e => setCurrentWord(e.target.value)}
+              value={currentWord}
+            />
+            <p>{currentAnswerStatus === UNATTEMPTED ? '' : currentAnswerStatus}</p>
+          </div>
 
-        <button
-          type="button"
-          onClick={handleCheck}
-          disabled={checkedDisable}
-        >
-          Check
-        </button>
+          <p>{currentAnswerStatus === WRONG ? wordList[wordCount] : ''}</p>
 
-        <button
-          type="button"
-          onClick={handleNext}
-          disabled={!checkedDisable || isComplete}
-        >
-          Next
-        </button>
+          <button
+            type="button"
+            onClick={handleCheck}
+            disabled={checkedDisable}
+          >
+            Check
+          </button>
+
+          <button
+            type="button"
+            onClick={handleNext}
+            disabled={!checkedDisable || isComplete}
+          >
+            Next
+          </button>
+        </div>
+        {
+          isComplete && (
+            <ul className={css.timeline}>
+              {
+                timeline.map(i => (
+                  <li key={i.word} className={i.isCorrect ? css['timeline-correct'] : css['timeline-wrong']}>{i.word}</li>
+                ))
+              }
+            </ul>
+          )
+        }
       </div>
     </div>
   );
